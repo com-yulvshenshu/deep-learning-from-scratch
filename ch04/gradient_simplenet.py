e0358aae414a769b059141c608985821061615b0
michael_sun <michael.sun@dyuanzi.com>
2019-10-08 17:25:13 +0800

    change for: code formate

--- ch04/gradient_simplenet.py
+++ ch04/gradient_simplenet.py
@@ -8,7 +8,7 @@ from common.gradient import numerical_gradient
 
 class simpleNet:
     def __init__(self):
-        self.W = np.random.randn(2,3)
+        self.W = np.random.randn(2, 3)
 
     def predict(self, x):
         return np.dot(x, self.W)
@@ -19,6 +19,7 @@ class simpleNet:
         loss = cross_entropy_error(y, t)
 
         return loss
+
 
 x = np.array([0.6, 0.9])
 t = np.array([0, 0, 1])
